1c99c55a3ecf89861614dc084ec8c1b030999582
Geffen Cooper <geffencooper@gmail.com>
2022-11-14 16:45:34 -0600

    fixed some bugs

--- src/domain_shift/scratch.py
+++ src/domain_shift/scratch.py
@@ -10,92 +10,103 @@ from torchvision import transforms
 import time
 import torchvision.models as models
 
-model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', weights='MobileNet_V2_Weights.DEFAULT')
-model.eval()
+# load models
+mobilenet = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', weights='MobileNet_V2_Weights.DEFAULT')
+mobilenet.eval()
 resnet18 = models.resnet18(weights='DEFAULT')
 resnet18.eval()
 
-fig, (ax0,ax1, ax2) = plt.subplots(1, 3,figsize=(15, 5),gridspec_kw={'width_ratios': [0.5,1.5, 3]})
-samp = Slider(ax0, 'amnt', 0, 3.0, valinit=1,orientation="vertical")
-axr = fig.add_axes([0.05, 0.7, 0.08, 0.15])
-axi = fig.add_axes([0.05, 0.5, 0.08, 0.15])
-radio = RadioButtons(axr, ('brightness', 'noise', 'sharpness'), active=0)
-radio2 = RadioButtons(axi, ('cup', 'sock', 'water bottle','backpack','live'), active=0)
+# init gui
+fig, (sev_ax,img_ax,acc_ax) = plt.subplots(1, 3,figsize=(15, 5),gridspec_kw={'width_ratios': [0.5,1.5, 3]})
+severity = Slider(sev_ax, 'amnt', 0, 3.0, valinit=1,orientation="vertical") # severity of corruptions
+corr_ax = fig.add_axes([0.05, 0.7, 0.08, 0.15]) # corruptions
+class_ax = fig.add_axes([0.05, 0.5, 0.08, 0.15]) # image type
+corr_radio = RadioButtons(corr_ax, ('brightness', 'noise', 'sharpness'), active=0)
+class_radio = RadioButtons(class_ax, ('cup', 'sock', 'water bottle','backpack','live'), active=0)
 
+
+# init video capture object
 cap = cv2.VideoCapture(0)
 
 # Check if camera opened successfully
 if (cap.isOpened()== False):
     print("Error opening video file")
 
-# unnormalize the tensor to visualize
-def get_og(input_tensor):
+# unnormalize the imagenet normalization and normalize to [0,1] to visualize
+def get_og(input_tensor,ren):
+    # get a copy
     input_tensor = copy.deepcopy(input_tensor).cpu()
+
+    # undo imagenet normalization
     input_tensor[:][:][0]*=(.229)
     input_tensor[:][:][0]+=(0.485)
     input_tensor[:][:][1]*=(.224)
     input_tensor[:][:][1]+=(0.455)
     input_tensor[:][:][2]*=(.225)
     input_tensor[:][:][2]+=(0.405)
-    input_tensor = (input_tensor-torch.min(input_tensor))/torch.max(input_tensor-torch.min(input_tensor))
+
+    # normalize to valid range [0,1]
+    if ren:
+        input_tensor = (input_tensor-torch.min(input_tensor))/torch.max(input_tensor-torch.min(input_tensor))
     return input_tensor
 
+# get imagenet classes
 with open("imagenet_classes.txt", "r") as f:
     categories = [s.strip() for s in f.readlines()]
 
+# preprocess imagenet inputs
 preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.ToTensor()
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-print("start")
-input_image = Image.open("cup.JPEG")
-og_input_tensor = preprocess(input_image)
-og_input_img = get_og(og_input_tensor)
-corr = "brightness"
-img_class = "cup"
+# on startup use cup by default
+glob_input_image = Image.open("cup.JPEG")
+glob_corr = "brightness"
+glob_img_class = "cup"
 
 
-def update(val):
-    global og_input_tensor
-    global og_input_img
-    global corr
-    global img_class
-
-    input_tensor = copy.deepcopy(og_input_tensor)
-    og_img = copy.deepcopy(og_input_img)
-    amp = samp.val
-    if corr == "brightness":
-        # print(input_tensor[0][0][0])
-        input_tensor = TF.adjust_brightness(input_tensor,amp)
-        # print(input_tensor[0][0][0])
-        og_img = TF.adjust_brightness(og_img,amp)
-    elif corr == "noise":
-        input_tensor += ((amp-1))*torch.randn((input_tensor.size()))
-        og_img += ((amp-1))*torch.randn(og_img.size())
-    elif corr == "sharpness":
-        input_tensor = TF.adjust_sharpness(input_tensor,amp)
-        og_img = TF.adjust_sharpness(og_img,amp)
-
-    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+def sev_update(val):
+    start = time.time()
+    # use global variables
+    global glob_input_image
+    global glob_corr
+    global glob_img_class
+    
+    amp = severity.val
+    inf_tensor = preprocess(glob_input_image)
+    
+    if glob_corr == "brightness":
+        inf_tensor = TF.adjust_brightness(inf_tensor,amp)
+        # viz_tensor = TF.adjust_brightness(viz_tensor,amp)
+    elif glob_corr == "noise":
+        inf_tensor += ((amp-1))*torch.randn(inf_tensor.size())
+        # viz_tensor += ((amp-1))*torch.randn(viz_tensor.size())
+    elif glob_corr == "sharpness":
+        inf_tensor = TF.adjust_sharpness(inf_tensor,amp)
+        # viz_tensor = TF.adjust_sharpness(viz_tensor,amp)
+    
+    inf_tensor = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(inf_tensor)
+    viz_tensor = get_og(inf_tensor,ren=(glob_corr=="noise" or glob_corr=="sharpness"))
+    input_batch = inf_tensor.unsqueeze(0) # create a mini-batch as expected by the model
 
     # move the input and model to GPU for speed if available
     if torch.cuda.is_available():
         input_batch = input_batch.to('cuda')
         # resnet18.to('cuda')
-        model.to('cuda')
+        mobilenet.to('cuda')
 
     with torch.no_grad():
         # output = resnet18(input_batch)
-        output = model(input_batch)
-    # Tensor of shape 1000, with confidence scores over Imagenet's 1000 classes
-    # print(output[0])
+        output = mobilenet(input_batch)
+        
+    
     # The output has unnormalized scores. To get probabilities, you can run a softmax on it.
     probabilities = torch.nn.functional.softmax(output[0], dim=0)
 
-    ax1.imshow(og_img.permute(1, 2, 0))
+    img_ax.imshow(viz_tensor.permute(1, 2, 0))
     
     # Show top categories per image
     top5_prob, top5_catid = torch.topk(probabilities, 5)
@@ -103,41 +114,45 @@ def update(val):
     for j in range(top5_prob.size(0)):
         labels.append(categories[top5_catid[j].cpu()])
     try:
-        idx = labels.index(img_class)
+        idx = labels.index(glob_img_class)
     except:
         idx=6
-    ax2.clear()
+    acc_ax.clear()
     colors = ['b']*5
     if idx < 5:
         colors[idx] = 'r'
-    ax2.bar([0,30,60,90,120],top5_prob.cpu(),tick_label=labels,width=25,color=colors)
-    ax2.set_ylim(0,1)
+    acc_ax.bar([0,30,60,90,120],top5_prob.cpu(),tick_label=labels,width=25,color=colors)
+    acc_ax.set_ylim(0,1)
     plt.draw()
-samp.on_changed(update)
+    # print(time.time()-start)
 
-def update2(val):
-    global corr
-    corr = val
-    update(1)
-radio.on_clicked(update2)
 
-def update3(val):
-    global img_class
-    global og_input_tensor
-    global og_input_img
-    img_class = val
+def corr_update(val):
+    global glob_corr
+    glob_corr = val
+    sev_update(val)
+
+
+def class_update(val):
+    global glob_img_class
+    global glob_input_image
+    glob_img_class = val
+
     # load image
-    if img_class == "live":
+    if glob_img_class == "live":
         ret, frame = cap.read()
         if ret == True:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            input_image = Image.fromarray(frame)
+            glob_input_image = Image.fromarray(frame)
     else:
-        input_image = Image.open(img_class+".JPEG")
-    og_input_tensor = preprocess(input_image)
-    og_input_img = get_og(og_input_tensor)
-    update(1)
-radio2.on_clicked(update3)
+        glob_input_image = Image.open(glob_img_class+".JPEG")
 
-update(1)
+    sev_update(1)
+
+
+severity.on_changed(sev_update)
+corr_radio.on_clicked(corr_update)
+class_radio.on_clicked(class_update)
+
+sev_update(1)
 plt.show()
